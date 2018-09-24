76b327d1950e5b1e3a1b1dfdef0d81bf0662fa80
unknown <itiann1234@gmail.com>
2018-09-25 01:55:07 +0800

    Created Send Complaint, Integrated Google Map, Created History, and Complaint Details

---
+++ app/src/main/java/com/trashguard/trashgaurd/View/ComplaintDetailsView.java
@@ -0,0 +1,15 @@
+package com.trashguard.trashgaurd.View;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.trash.trashguard.R;
+
+public class ComplaintDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_complaint_details);
+    }
+}
---
+++ app/src/main/java/com/trashguard/trashgaurd/View/ComplaintHistoryView.java
@@ -0,0 +1,13 @@
+package com.trash.trashguard;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ComplaintHistoryView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_complaint_history);
+    }
+}
