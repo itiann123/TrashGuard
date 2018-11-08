6555bcf7691763f46411839b26336b7fbde1b352
unknown <itiann1234@gmail.com>
2018-11-08 16:43:56 +0800

    Added Device Location

--- app/src/main/java/com/trashguard/trashgaurd/Presenter/SignUpPresenter.java
+++ app/src/main/java/com/trashguard/trashgaurd/Presenter/SignUpPresenter.java
@@ -5,16 +5,15 @@ import android.util.Patterns;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
+
 import com.trashguard.trashgaurd.Model.UserModel;
 import com.trashguard.trashgaurd.Presenter.Interaface.ISignUpPresenter;
 import com.trashguard.trashgaurd.View.Interface.ISignUpView;
 import com.trashguard.trashgaurd.View.SignUpView;
 
-public class SignUpPresenter implements ISignUpPresenter {
+public class SignUpPresenter  {
+//implements ISignUpPresenter
 
-    private FirebaseAuth mAuth;
     private ISignUpView iSignUpView;
     private SignUpView signUpView;
 
@@ -23,56 +22,56 @@ public class SignUpPresenter implements ISignUpPresenter {
     }
 
 
-    @Override
-    public void registerUser(final String email, final String password) {
+//    @Override
+//    public void registerUser(final String email, final String password) {
+//
+//        signUpView  = new SignUpView();
+//        mAuth = FirebaseAuth.getInstance();
+//
+//        if(email.isEmpty()){
+//            SignUpView.email_field.setError("Email is required");
+//            SignUpView.email_field.requestFocus();
+//            return;
+//        }
+//
+//        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+//            SignUpView.email_field.setError("Please enter a valid email");
+//            SignUpView.email_field.requestFocus();
+//            return;
+//        }
+//
+//        if(password.isEmpty()){
+//            SignUpView.password_field.setError("Password is required");
+//            SignUpView.password_field.requestFocus();
+//            return;
+//        }
+//
+//        if(password.length() < 6){
+//            SignUpView.password_field.setError("Minimum length of password should be 6");
+//            SignUpView.password_field.requestFocus();
+//            return;
+//
+//        }
+//
+//        mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+//            @Override
+//            public void onComplete(@NonNull Task<AuthResult> task) {
+//                if (task.isSuccessful()) {
+//                    onSignUp(email, password);
+//                }
+//            }
+//        });
+//    }
 
-        signUpView = new SignUpView();
-        mAuth = FirebaseAuth.getInstance();
-
-        if(email.isEmpty()){
-            SignUpView.email_field.setError("Email is required");
-            SignUpView.email_field.requestFocus();
-            return;
-        }
-
-        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
-            SignUpView.email_field.setError("Please enter a valid email");
-            SignUpView.email_field.requestFocus();
-            return;
-        }
-
-        if(password.isEmpty()){
-            SignUpView.password_field.setError("Password is required");
-            SignUpView.password_field.requestFocus();
-            return;
-        }
-
-        if(password.length() < 6){
-            SignUpView.password_field.setError("Minimum length of password should be 6");
-            SignUpView.password_field.requestFocus();
-            return;
-
-        }
-
-        mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-            @Override
-            public void onComplete(@NonNull Task<AuthResult> task) {
-                if (task.isSuccessful()) {
-                    onSignUp(email, password);
-                }
-            }
-        });
-    }
-
-    @Override
-    public void onSignUp(String email, String password) {
-        UserModel user = new UserModel(password, email);
-
-        boolean isSignUpSuccess = user.isValidData();
-
-        if(isSignUpSuccess)
-            iSignUpView.onSignUpResult("Sign up Success");
-        else
-            iSignUpView.onSignUpResult("Sign up Error");
-    }
+//    @Override
+//    public void onSignUp(String email, String password) {
+//        UserModel user = new UserModel(password, email);
+//
+//        boolean isSignUpSuccess = user.isValidData();
+//
+//        if(isSignUpSuccess)(
+//            iSignUpView.onSignUpResult("Sign up Success");
+//        else
+//            iSignUpView.onSignUpResult("Sign up Error");
+//    }
 }
--- app/src/main/java/com/trashguard/trashgaurd/View/HomeView.java
+++ app/src/main/java/com/trashguard/trashgaurd/View/HomeView.java
@@ -1,31 +1,42 @@
 package com.trashguard.trashgaurd.View;
 
 import android.Manifest;
-import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Build;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.Tasks;
 import com.trash.trashguard.R;
 
-public class HomeView extends AppCompatActivity implements OnMapReadyCallback {
+public class HomeView extends AppCompatActivity implements OnMapReadyCallback, View.OnClickListener {
     private static final int MY_REQUEST_INT = 177;
+    private static final String TAG = "MapActivity";
+    private static final String FINE_LOCATION = Manifest.permission.ACCESS_FINE_LOCATION;
+    private static final String COURSE_LOCATION = Manifest.permission.ACCESS_COARSE_LOCATION;
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
+    private static final float DEFAULT_ZOOM = 15f;
     private GoogleMap mMap;
     private ImageButton add_complain;
     private Toolbar toolbar;
@@ -33,60 +44,125 @@ public class HomeView extends AppCompatActivity implements OnMapReadyCallback {
     private ImageView history_button;
     private ImageView home_button;
     private ImageView notifications_button;
+    private FusedLocationProviderClient mFusedLocationProviderClient;
+    private Boolean mLocationPermissionsGranted = false;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        initButtons();
+        initMap();
+//        getLocationPermission();
+
+    }
+
+    private void initButtons() {
+        Log.d(TAG, "initButtons: Button Initialized");
+        home_button = (ImageView) findViewById(R.id.home);
+        history_button = (ImageView) findViewById(R.id.history);
+        add_complain = (ImageButton) findViewById(R.id.add_complain);
+        notifications_button = (ImageView) findViewById(R.id.notification);
+        profilepic = (ImageView) findViewById(R.id.profile_pic4);
+
+    }
+
+
+    private void initMap() {
+        Log.d(TAG, "initMap: initializing map");
+        Toast.makeText(this, "Map is ready", Toast.LENGTH_SHORT).show();
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-
-        home_button = (ImageView) findViewById(R.id.home);
-        home_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(HomeView.this, "Already home.", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        history_button = (ImageView) findViewById(R.id.history);
-        history_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i = new Intent(HomeView.this, ComplaintHistoryView.class);
-                startActivity(i);
-            }
-        });
-        add_complain = (ImageButton) findViewById(R.id.add_complain);
-        add_complain.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i = new Intent(HomeView.this, AddComplainView.class);
-                startActivity(i);
-            }
-        });
-
-        notifications_button = (ImageView) findViewById(R.id.notification);
-        notifications_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i = new Intent(HomeView.this, NotificationsView.class);
-                startActivity(i);
-            }
-        });
-
-        profilepic = (ImageView) findViewById(R.id.profile_pic4);
-        profilepic.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(HomeView.this,ProfileView.class);
-                startActivity(i);
-            }
-        });
     }
 
+    private void getDeviceLocation() {
+        Log.d(TAG, "getDeviceLocation: getting device location");
+
+        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+
+        try {
+            Task<Location> location = mFusedLocationProviderClient.getLastLocation();
+            location.addOnCompleteListener(new OnCompleteListener<Location>() {
+                @Override
+                public void onComplete(@NonNull Task<Location> task) {
+                    if (task.isSuccessful() && task.getResult() != null) {
+                        Log.d(TAG, "onComplete: Found device location");
+                        Location currentLocation = (Location) task.getResult();
+
+                        moveCamera(new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude()), DEFAULT_ZOOM);
+                    } else {
+                        Log.d(TAG, "onComplete: current locations is null");
+                        Toast.makeText(HomeView.this, "Unable to get current location", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+
+        } catch (SecurityException e) {
+            Log.d(TAG, "getDeviceLocation: Security Exception" + e.getMessage());
+        }
+    }
+
+    private void moveCamera(LatLng latLng, float zoom) {
+        Log.d(TAG, "moveCamera: moving camera to: lat:" + latLng.latitude + ",long :" + latLng.longitude);
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, zoom));
+    }
+
+//    private void getLocationPermission(){
+//        Log.d(TAG,"getLocationPermission: getting location permission");
+//        String[] permissions = {Manifest.permission .ACCESS_FINE_LOCATION,
+//        Manifest.permission.ACCESS_COARSE_LOCATION};
+//        if (Build.VERSION.SDK_INT >= 23) {
+//            if (ContextCompat.checkSelfPermission(HomeView.this,
+//                    FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+//                Log.d(TAG, "PERMISSION: FINE_LOCATION GRANTED");
+//                if (ContextCompat.checkSelfPermission(HomeView.this,
+//                        COURSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+//                    Log.d(TAG, "PERMISSION: COURSE_LOCATIOn GRANTED");
+//                    mLocationPermissionsGranted = true;
+//                } else {
+//                    Log.d(TAG, "Inner If: PERMISSION ASKED");
+//                    ActivityCompat.requestPermissions(HomeView.this,
+//                            permissions,
+//                            LOCATION_PERMISSION_REQUEST_CODE);
+//                }
+//            } else {
+//                Log.d(TAG, "Outer     If: PERMISSION ASKED");
+//                ActivityCompat.requestPermissions(HomeView.this,
+//                        permissions,
+//                        LOCATION_PERMISSION_REQUEST_CODE);
+//
+//            }
+//        }else
+//            Log.d(TAG,"SDK NOT VALID");
+//    }
+//
+//    @Override
+//    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+//        Log.d(TAG,"onRequestPermissionResult: called");
+//        mLocationPermissionsGranted = false;
+//
+//        switch (requestCode){
+//            case LOCATION_PERMISSION_REQUEST_CODE:{
+//                if(grantResults.length > 0) {
+//                    for(int i =0 ; i < grantResults.length; i++) {
+//                        if(grantResults[i] != PackageManager.PERMISSION_GRANTED){
+//                            mLocationPermissionsGranted = false;
+//                            Log.d(TAG,"onRequestPermissionResult: permission failed");
+//                            return;
+//                        }
+//                    }
+//                    Log.d(TAG,"onRequestPermissionResult: permission granted");
+//                    mLocationPermissionsGranted = true;
+//                    //initialize map
+////                    initMap();
+//                }
+//            }
+//        }
+//    }
 
     /**
      * Manipulates the map once available.
@@ -99,13 +175,25 @@ public class HomeView extends AppCompatActivity implements OnMapReadyCallback {
      */
     @Override
     public void onMapReady(GoogleMap googleMap) {
+        Log.d(TAG, "onMapReady: map is ready");
         mMap = googleMap;
 
-
-        // Add a marker in Sydney and move the camera
-        LatLng house = new LatLng(10.299506, 123.870354);
-        mMap.addMarker(new MarkerOptions().position(house).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(house));
+//        // Add a marker in Sydney and move the camera
+//        LatLng house = new LatLng(10.299506, 123.870354);
+//        mMap.addMarker(new MarkerOptions().position(house).title("Marker in Sydney"));
+//        mMap.moveCamera(CameraUpdateFactory.newLatLng(house));
+        getDeviceLocation();
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        mMap.setMyLocationEnabled(true);
     }
 
     @Override
@@ -115,5 +203,56 @@ public class HomeView extends AppCompatActivity implements OnMapReadyCallback {
         mainActivity.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         startActivity(mainActivity);
         finish();
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.add_complain:
+                    add_complain.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            Intent i = new Intent(HomeView.this, AddComplainView.class);
+                            startActivity(i);
+                        }
+                    });
+                    break;
+
+            case R.id.notification:
+                    notifications_button.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            Intent i = new Intent(HomeView.this, NotificationsView.class);
+                            startActivity(i);
+                        }
+                    });
+                    break;
+            case R.id.profile_pic:
+                    profilepic.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            Intent i = new Intent(HomeView.this,ProfileView.class);
+                            startActivity(i);
+                        }
+                    });
+                    break;
+            case R.id.history:
+                    history_button.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            Intent i = new Intent(HomeView.this, ComplaintHistoryView.class);
+                            startActivity(i);
+                        }
+                    });
+                    break;
+            case R.id.home:
+                    home_button.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            Toast.makeText(HomeView.this, "Already home.", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                    break;
+        }
     }
 }
